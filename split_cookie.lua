2d25989f311257a9b83dc504225d042ec8e4f76f
Masahiro Nagano <kazeburo@gmail.com>
2013-11-11 14:56:16 +0900

    remove unused lua

--- split_cookie.lua
+++
@@ -1,7 +0,0 @@
-if ngx.var.cookie_isucon_session then 
-    local t = split(ngx.unescape_uri(ngx.var.cookie_isucon_session), ",")
-    if t[2] then
-        ngx.var.isucon_username = t[2]
-        ngx.var.isucon_token = t[3]
-    end
-end
